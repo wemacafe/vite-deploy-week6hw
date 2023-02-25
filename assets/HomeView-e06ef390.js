3d1e21bb3a7ebfd17222fe3295e7d2a7bb67d097
amber.chang.w <amber.chang@mail.jmg.com.tw>
2023-02-25 17:17:24 +0800

    deploy

---
+++ assets/HomeView-e06ef390.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as s}from"./index-eac41100.js";const t={components:{}},c={class:"home"};function n(r,a,_,p,i,m){return s(),o("div",c," 這是首頁 ")}const f=e(t,[["render",n]]);export{f as default};
